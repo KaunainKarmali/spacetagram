64a7722ee9223ec220825ab79f63886db0c591bc
Kaunain <kaunaink@gmail.com>
2021-09-21 23:35:48 -0400

    minor api key fix

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,5 +1,5 @@
 const api = {};
-api.key = "PLzloo5BJxcBKeLhRqmMWraAKqaHBrHWQbGeuf0X1";
+api.key = "PLzloo5BJxcBKeLhRqmMWraAKqaHBrHWQbGeuf0X";
 api.url = "https://api.nasa.gov/planetary/apod";
 export { api };
 
